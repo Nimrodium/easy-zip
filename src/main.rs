2edb4cfb2351d6a4f2dc3724b9ae250c9f70247c
nimrod <nimrodium@protonmail.com>
2026-01-11 15:38:46 -0600

    reorganized

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod formats;
 #[macro_use]
 mod utils;
 use crate::formats::{ArchiveFormat, Format, Options};
+use utils::{infer, Mode};
 const NAME: &str = "sticky";
 static mut VERBOSE: bool = false;
 
@@ -35,57 +36,10 @@ struct Args {
     verbose: bool,
 }
 
-#[derive(Debug, Clone)]
-enum Mode {
-    Compress,
-    Extract,
-}
-
 fn infer_format(archive: &Path) -> Option<Format> {
     utils::extract_file_extension(archive)
         .map(|ext| Format::from_str(&ext).ok())
         .unwrap_or(None)
-}
-fn infer(sources: &[PathBuf], target: &Option<PathBuf>) -> (Option<Mode>, Option<Format>) {
-    let files = sources.iter().filter(|p| p.is_file());
-    // if any directories are present, must be compress. if any valid formats are present in the files, then ambigious.
-    let dirs = sources.iter().map(|f| f.is_dir()).filter(|d| *d).count();
-
-    // if any valid formats are present, must be extract. else compress. if any non-archive files present, then ambigious
-    // let file_formats = files.map(|(ext, _)| Format::from_str(&ext).ok());
-    let file_formats =
-        files.map(|f| utils::extract_file_extension(f).and_then(|ext| Format::from_str(&ext).ok()));
-    let non_archive = file_formats.clone().filter(|f| f.is_none()).count();
-    let formats = file_formats
-        .filter(|f| f.is_some())
-        .map(|f| f.unwrap())
-        .collect::<Vec<Format>>();
-    let format = if formats.iter().all(|f| Some(f) == formats.get(0)) {
-        formats.get(0).cloned()
-    } else {
-        None
-    };
-    let archive_count = formats.len();
-    let target_format = target
-        .as_ref()
-        .map(|t| Format::format_from_path(t))
-        .unwrap_or(None);
-    // if only directories are present, then must be compress
-    // if no archives are present, must be compress
-    // if only archives are present, must be extract
-    // if archives exist with any other files/dirs, ambigious
-    let ambigious = (None, None);
-    let compress = Some(Mode::Compress);
-    let extract = Some(Mode::Extract);
-
-    match (archive_count != 0, dirs != 0, non_archive != 0) {
-        (true, true, true) => ambigious,
-        (true, true, false) => ambigious,
-        (true, false, true) => ambigious,
-        (false, false, false) => ambigious,
-        (true, false, false) => (extract, format),
-        _ => (compress, target_format),
-    }
 }
 
 fn main() -> Result<(), String> {
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,9 @@
-use std::path::Path;
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+
+use crate::formats::Format;
 
 pub fn extract_file_extension(path: &Path) -> Option<String> {
     // does not use p.extension as i want to catch .tar.gz
@@ -17,6 +22,53 @@ pub fn extract_file_extension(path: &Path) -> Option<String> {
 
     ext.map(|ext| ext.chars().rev().collect::<String>())
 }
+#[derive(Debug, Clone)]
+pub enum Mode {
+    Compress,
+    Extract,
+}
+pub fn infer(sources: &[PathBuf], target: &Option<PathBuf>) -> (Option<Mode>, Option<Format>) {
+    let files = sources.iter().filter(|p| p.is_file());
+    // if any directories are present, must be compress. if any valid formats are present in the files, then ambigious.
+    let dirs = sources.iter().map(|f| f.is_dir()).filter(|d| *d).count();
+
+    // if any valid formats are present, must be extract. else compress. if any non-archive files present, then ambigious
+    // let file_formats = files.map(|(ext, _)| Format::from_str(&ext).ok());
+    let file_formats =
+        files.map(|f| extract_file_extension(f).and_then(|ext| Format::from_str(&ext).ok()));
+    let non_archive = file_formats.clone().filter(|f| f.is_none()).count();
+    let formats = file_formats
+        .filter(|f| f.is_some())
+        .map(|f| f.unwrap())
+        .collect::<Vec<Format>>();
+    let format = if formats.iter().all(|f| Some(f) == formats.get(0)) {
+        formats.get(0).cloned()
+    } else {
+        None
+    };
+    let archive_count = formats.len();
+    let target_format = target
+        .as_ref()
+        .map(|t| Format::format_from_path(t))
+        .unwrap_or(None);
+    // if only directories are present, then must be compress
+    // if no archives are present, must be compress
+    // if only archives are present, must be extract
+    // if archives exist with any other files/dirs, ambigious
+    let ambigious = (None, None);
+    let compress = Some(Mode::Compress);
+    let extract = Some(Mode::Extract);
+
+    match (archive_count != 0, dirs != 0, non_archive != 0) {
+        (true, true, true) => ambigious,
+        (true, true, false) => ambigious,
+        (true, false, true) => ambigious,
+        (false, false, false) => ambigious,
+        (true, false, false) => (extract, format),
+        _ => (compress, target_format),
+    }
+}
+
 #[macro_export]
 macro_rules! error {
     () => {
