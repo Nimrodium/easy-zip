90c18bb663fe82f68622e04e82f25ea90e337289
nimrod <nimrodium@protonmail.com>
2026-01-12 00:02:55 -0600

    removed debug prints

--- src/utils.rs
+++ src/utils.rs
@@ -8,7 +8,7 @@ use std::{
 pub fn extract_file_extension(path: &Path) -> Option<String> {
     // does not use p.extension as i want to catch .tar.gz
     let name = path.file_name().and_then(|p| Some(p.to_string_lossy()));
-    println!("{name:?}");
+    // println!("{name:?}");
     let ext: Option<String> = name
         .as_ref()
         .and_then(|n| {
@@ -17,7 +17,7 @@ pub fn extract_file_extension(path: &Path) -> Option<String> {
                 .and_then(|i| if i == 0 { None } else { Some(i) }) // stop for hidden files
                 .and_then(|i| Some(n.len() - i - 1))
         })
-        .inspect(|i| println!("{i}"))
+        // .inspect(|i| println!("{i}"))
         .and_then(|i| name.and_then(|n| Some(n.chars().rev().take(i).collect())));
 
     ext.map(|ext| ext.chars().rev().collect::<String>())
