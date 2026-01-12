0c8c12cf8d4ea92dcf271a1c6d9704dadff8ead9
nimrod <nimrodium@protonmail.com>
2026-01-11 23:17:41 -0600

    added tar.gz support

--- src/formats/targz.rs
+++ src/formats/targz.rs
@@ -1,3 +1,8 @@
+use std::fs::File;
+
+use flate2::{read::GzDecoder, write::GzEncoder, Compression};
+use tar::Archive;
+
 use crate::formats::{ArchiveFormat, Options};
 
 #[derive(Debug, Clone, PartialEq, Eq, Copy)]
@@ -10,10 +15,40 @@ impl ArchiveFormat for TarGz {
         archive: &std::path::Path,
         options: Options,
     ) -> Result<(), String> {
-        todo!()
+        let file =
+            File::create(archive).map_err(|e| format!("could not create zip archive {e}"))?;
+        let compression = if let Some(c) = options.compression_level {
+            Compression::new(c as u32)
+        } else {
+            Compression::default()
+        };
+        let enc = GzEncoder::new(file, compression);
+        let mut tar = tar::Builder::new(enc);
+        for source in sources {
+            if source.is_dir() {
+                tar.append_dir_all("", source)
+                    .map_err(|e| format!("failed to add source to archive {e}"))?;
+            } else {
+                tar.append_path(source)
+                    .map_err(|e| format!("could not archive source file {source:?}: {e}"))?;
+            }
+        }
+        tar.finish()
+            .map_err(|e| format!("failed to complete write to {archive:?} tar.gz archive: {e}"))?;
+
+        Ok(())
     }
 
-    fn extract(&self, archive: &std::path::Path, target: &std::path::Path) -> Result<(), String> {
-        todo!()
+    fn extract(
+        &self,
+        archive_file: &std::path::Path,
+        target: &std::path::Path,
+    ) -> Result<(), String> {
+        let file = File::open(archive_file).map_err(|e| format!("could not open archive {e}"))?;
+        let mut archive = Archive::new(GzDecoder::new(file));
+        archive.unpack(target).map_err(|e| {
+            format!("failed to unpack tar.gz archive {archive_file:?} -> {target:?}: {e}")
+        })?;
+        Ok(())
     }
 }
